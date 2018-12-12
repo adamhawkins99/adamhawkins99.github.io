ab98e2cac53a418db89125218c15d8c755b9b278
CIT 230 Adam Hawkins <adamhawkins99@gmail.com>
2018-12-12 06:56:31 -0800

    Trying to fix weatherapi

--- assignments/templeinn/weatherapicc.js
+++ assignments/templeinn/weatherapicc.js
@@ -1,10 +1,10 @@
-let weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5536630&APPID=c6f31eed39521edf3d22b69d4b52f552&units=imperial', true);
+let weatherRequestcc = new XMLHttpRequest();
+weatherRequestcc.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5536630&APPID=c6f31eed39521edf3d22b69d4b52f552&units=imperial', true);
 
-weatherRequest.send();
+weatherRequestcc.send();
 
-weatherRequest.onload = function () {
-    let weatherData = JSON.parse(weatherRequest.responseText);
+weatherRequestcc.onload = function () {
+    let weatherData = JSON.parse(weatherRequestcc.responseText);
     console.log(weatherData);
     document.getElementById("placecc").innerHTML = weatherData.name;
     document.getElementById("weatherDesccc").innerHTML = weatherData.weather[0].main;
