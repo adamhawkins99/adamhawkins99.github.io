1874037ca577d3e6a2d4a7b289fe2f17524f0229
CIT 230 Adam Hawkins <adamhawkins99@gmail.com>
2018-12-12 06:59:20 -0800

    still trying

--- assignments/templeinn/weatherapila.js
+++ assignments/templeinn/weatherapila.js
@@ -1,10 +1,10 @@
-let weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5368381&APPID=c6f31eed39521edf3d22b69d4b52f552&units=imperial', true);
+let weatherRequestla = new XMLHttpRequest();
+weatherRequestla.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5368381&APPID=c6f31eed39521edf3d22b69d4b52f552&units=imperial', true);
 
-weatherRequest.send();
+weatherRequestla.send();
 
-weatherRequest.onload = function () {
-    let weatherData = JSON.parse(weatherRequest.responseText);
+weatherRequestla.onload = function () {
+    let weatherData = JSON.parse(weatherRequestla.responseText);
     console.log(weatherData);
     document.getElementById("placela").innerHTML = weatherData.name;
     document.getElementById("weatherDescla").innerHTML = weatherData.weather[0].main;
--- assignments/templeinn/weatherapisg.js
+++ assignments/templeinn/weatherapisg.js
@@ -1,10 +1,10 @@
 let weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', '//api.openweathermap.org/data/2.5/weather?id=4171010&APPID=c6f31eed39521edf3d22b69d4b52f552&units=imperial', true);
+weatherRequestsg.open('GET', '//api.openweathermap.org/data/2.5/weather?id=4171010&APPID=c6f31eed39521edf3d22b69d4b52f552&units=imperial', true);
 
-weatherRequest.send();
+weatherRequestsg.send();
 
-weatherRequest.onload = function () {
-    let weatherData = JSON.parse(weatherRequest.responseText);
+weatherRequestsg.onload = function () {
+    let weatherData = JSON.parse(weatherRequestsg.responseText);
     console.log(weatherData);
     document.getElementById("placesg").innerHTML = weatherData.name;
     document.getElementById("weatherDescsg").innerHTML = weatherData.weather[0].main;
