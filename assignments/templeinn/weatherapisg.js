cfb640078be79213c4c7379570f8d6a500e1a390
CIT 230 Adam Hawkins <adamhawkins99@gmail.com>
2018-12-12 07:04:12 -0800

    Checking SG fix

--- assignments/templeinn/weatherapisg.js
+++ assignments/templeinn/weatherapisg.js
@@ -1,4 +1,4 @@
-let weatherRequest = new XMLHttpRequest();
+let weatherRequestsg = new XMLHttpRequest();
 weatherRequestsg.open('GET', '//api.openweathermap.org/data/2.5/weather?id=4171010&APPID=c6f31eed39521edf3d22b69d4b52f552&units=imperial', true);
 
 weatherRequestsg.send();
@@ -8,10 +8,8 @@ weatherRequestsg.onload = function () {
     console.log(weatherData);
     document.getElementById("placesg").innerHTML = weatherData.name;
     document.getElementById("weatherDescsg").innerHTML = weatherData.weather[0].main;
-    document.getElementById("highTempsg").innerHTML = weatherData.main.temp_max;
-    document.getElementById("lowTempsg").innerHTML = weatherData.main.temp_min;
     document.getElementById("current-tempsg").innerHTML = weatherData.main.temp;
-    document.getElementById("currentWindsg").innerHTML = weatherData.wind.speed;
+ 
 
     var iconcode = weatherData.weather[0].icon;
 
